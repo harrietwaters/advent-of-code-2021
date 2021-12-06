d13d9453abf6b223a2213ac6a182024a8172ce31
Harold Waters <harold.waters@flosports.tv>
2021-12-06 11:25:33 -0800

    day six refactor, now even sexier

--- day-six/day-six-part-two.js
+++ day-six/day-six-part-two.js
@@ -3,16 +3,9 @@ const { readFileSync } = require('fs')
 const rawInput = readFileSync('./input.txt').toString()
 
 function getStartingFish(input) {
-    const rawFishes = input
-        .split(',')
-        .map(str => +str)
-        .sort()
-
+    const rawFishes = input.split(',')
     const fish = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
-    for (const rawFish of rawFishes) {
-        fish[rawFish]++
-    }
+    for (const rawFish of rawFishes) fish[+rawFish]++
     return fish
 }
 
@@ -21,13 +14,9 @@ const startingFishes = getStartingFish(rawInput)
 function getFishPopulation(fishes, days) {
     for (let i = 0; i < days; i++) {
         const spawningFish = fishes.shift()
-        if (fishes.length < 9) fishes[8] = 0
-        if (spawningFish > 0) {
-            fishes[6] += spawningFish
-            fishes[8] = spawningFish
-        }
+        fishes[6] += spawningFish
+        fishes[8] = spawningFish
     }
-
     return fishes.reduce((acc, curr) => acc + curr, 0)
 }
 
