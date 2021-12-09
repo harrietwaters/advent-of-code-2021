1d0855b3419c477388bd1b18143bfb19b10e3ecd
Harold Waters <harold.waters@flosports.tv>
2021-12-08 22:21:44 -0600

    cannot stop refactoring!

--- day-eight/day-eight-part-two.js
+++ day-eight/day-eight-part-two.js
@@ -2,7 +2,7 @@ const { readFileSync } = require('fs')
 
 const rawInput = readFileSync('./input.txt').toString()
 
-const masks = {
+const bitmasks = {
     a: 0b01000000,
     b: 0b00100000,
     c: 0b00010000,
@@ -13,17 +13,16 @@ const masks = {
 }
 
 function wireToBitmask(wire) {
-    return wire.split('').reduce((acc, ltr) => acc | masks[ltr], 0)
+    return wire.split('').reduce((acc, ltr) => acc | bitmasks[ltr], 0)
 }
 
 function displayValue(activeWires, sortedWires) {
-    return +activeWires.reduce(
-        (output, wire) => output + sortedWires[wireToBitmask(wire)].toString(),
-        '',
-    )
+    return +activeWires
+        .map(wire => sortedWires[wireToBitmask(wire)])
+        .join('')
 }
 
-function sortWires(wires) {
+function wireDecoder(wires) {
     const sortedArr = new Array(10)
     let unsortedWires = new Array(10)
     for (const wire of wires) {
@@ -44,21 +43,19 @@ function sortWires(wires) {
     const nineIdx = unsortedWires.findIndex(wMask => (wMask | sortedArr[4]) === wMask);
     sortedArr[9] = unsortedWires.splice(nineIdx, 1)[0];
 
-    const bdWires = sortedArr[4] ^ sortedArr[7];
+    const bdBits = sortedArr[4] ^ sortedArr[7];
     const fiveIdx = unsortedWires
-        .findIndex(wMask => (wMask | sortedArr[9]) === sortedArr[9] && (wMask | bdWires) === wMask);
+        .findIndex(wMask => (wMask | sortedArr[9]) === sortedArr[9] && (wMask | bdBits) === wMask);
     sortedArr[5] = unsortedWires.splice(fiveIdx, 1)[0];
 
-    const eWire = sortedArr[8] ^ sortedArr[9];
-    sortedArr[6] = sortedArr[5] | eWire;
+    const eBit = sortedArr[8] ^ sortedArr[9];
+    sortedArr[6] = sortedArr[5] | eBit;
     unsortedWires = unsortedWires.filter(wMask => wMask !== sortedArr[6]);
 
-    const threeIdx = unsortedWires
-        .findIndex(wMask => (wMask | sortedArr[9]) === sortedArr[9]);
+    const threeIdx = unsortedWires.findIndex(wMask => (wMask | sortedArr[9]) === sortedArr[9]);
     sortedArr[3] = unsortedWires.splice(threeIdx, 1)[0];
 
-    const zeroIdx = unsortedWires
-        .findIndex(wMask => (wMask | sortedArr[7]) === wMask);
+    const zeroIdx = unsortedWires.findIndex(wMask => (wMask | sortedArr[7]) === wMask);
     sortedArr[0] = unsortedWires.splice(zeroIdx, 1)[0];
     sortedArr[2] = unsortedWires[0];
 
@@ -74,8 +71,8 @@ function parseInput(input) {
             const [rawWires, rawActive] = str.split(' | ')
             const wires = rawWires.split(' ')
             const active = rawActive.split(' ')
-            const sortedWires = sortWires(wires)
-            return displayValue(active, sortedWires)
+            const decoder = wireDecoder(wires)
+            return displayValue(active, decoder)
         })
 }
 
